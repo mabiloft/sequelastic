38399fa1144ed4d53469728d36611fdb47f39585
Daniele | Mabiloft <daniele@mabiloft.com>
2021-05-20 09:04:30 +0200

    added custom way to index models and search function

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,50 @@
-// require("dotenv-flow").config();
+import "colors";
 import elasticSearch, { ClientOptions } from "@elastic/elasticsearch";
 import pluralize from "pluralize";
 import { Model } from "sequelize-typescript";
-import "colors";
-type SequelasticConstructorProps = { models: typeof Model[] } & ClientOptions;
 
+type SequelasticModelType = {
+  model: typeof Model;
+  attributes?: string[] | { exclude: string[] };
+};
+type SequelasticConstructorProps = {
+  models: (typeof Model | SequelasticModelType)[];
+  exclude?: string[];
+} & ClientOptions;
+type SequelasticSearchOptionsWholeResponse = {
+  fuzzy?: boolean;
+  fuzziness: "AUTO" | number;
+  wholeResponse?: true;
+};
+type SequelasticSearchOptions = {
+  fuzzy?: boolean;
+  fuzziness: "AUTO" | number;
+  wholeResponse?: false;
+};
+
+function isSequelasticModel(
+  inputModel: any
+): inputModel is SequelasticModelType {
+  return !!(inputModel as SequelasticModelType).model;
+}
 export default class Sequelastic {
   public elastic: elasticSearch.Client;
-  public models: typeof Model[];
-
+  public models: (SequelasticModelType | typeof Model)[];
+  #fieldsToExclude: string[];
   // constructor(options: SqlasticConstructorProps) {
   constructor(options: SequelasticConstructorProps) {
-    const { models, ...cliOpt } = options;
+    const { models, exclude: fieldsToExclude, ...cliOpt } = options;
     this.models = models;
     this.elastic = new elasticSearch.Client({
       node: "http://localhost.mabiloft.com:9200",
       ...cliOpt,
     });
+    this.#fieldsToExclude = fieldsToExclude;
   }
 
   public async sync(): Promise<any> {
+    const toExclude = this.#fieldsToExclude;
+
     const allDbPromises: Promise<any>[] = [];
     const allIndiciesCreationPromises: Promise<any>[] = [];
     try {
@@ -30,19 +55,36 @@ export default class Sequelastic {
       throw new Error(err);
     }
 
-    this.models.forEach((model: typeof Model) => {
+    this.models.forEach((model: SequelasticModelType) => {
       allIndiciesCreationPromises.push(
         new Promise((res, rej) => {
           // console.log("Model", model.rawAttributes);
           try {
-            this.elastic.indices.create({
-              index: pluralize.plural(model.name.toLowerCase()),
-              body: {
-                mappings: {
-                  properties: parseSqlAttributesToElastic(model.rawAttributes),
+            if (isSequelasticModel(model)) {
+              this.elastic.indices.create({
+                index: pluralize.plural(model.model.name.toLocaleLowerCase()),
+                body: {
+                  mappings: {
+                    properties: parseSqlAttributesToElastic(
+                      model.model.rawAttributes
+                    ),
+                  },
                 },
-              },
-            });
+              });
+            } else {
+              this.elastic.indices.create({
+                index: pluralize.plural(
+                  (model as typeof Model).name.toLowerCase()
+                ),
+                body: {
+                  mappings: {
+                    properties: parseSqlAttributesToElastic(
+                      (model as typeof Model).rawAttributes
+                    ),
+                  },
+                },
+              });
+            }
           } catch (err) {
             throw new Error(err);
           }
@@ -54,23 +96,47 @@ export default class Sequelastic {
       allDbPromises.push(
         new Promise(function (res, rej) {
           try {
-            res(
-              model
-                .unscoped()
-                .findAll()
-                .map((x: any) => {
-                  // console.log("PROMISE", x.toJSON());
+            if (!!model.model) {
+              res(
+                model.model
+                  .unscoped()
+                  .findAll({
+                    attributes: model.attributes
+                      ? model.attributes
+                      : { exclude: toExclude },
+                  })
+                  .map((x: any) => {
+                    // console.log("PROMISE", x.toJSON());
 
-                  return [
-                    {
-                      index: {
-                        _index: pluralize.plural(model.name.toLowerCase()),
+                    return [
+                      {
+                        index: {
+                          _index: pluralize.plural(model.name.toLowerCase()),
+                        },
                       },
-                    },
-                    x?.toJSON(),
-                  ];
-                })
-            );
+                      x?.toJSON(),
+                    ];
+                  })
+              );
+            } else {
+              res(
+                model
+                  .unscoped()
+                  .findAll({ attributes: { exclude: toExclude } })
+                  .map((x: any) => {
+                    // console.log("PROMISE", x.toJSON());
+
+                    return [
+                      {
+                        index: {
+                          _index: pluralize.plural(model.name.toLowerCase()),
+                        },
+                      },
+                      x?.toJSON(),
+                    ];
+                  })
+              );
+            }
           } catch (err) {
             rej(err);
           }
@@ -80,25 +146,104 @@ export default class Sequelastic {
 
     try {
       const allDocuments = await Promise.all(allDbPromises);
-      // console.log("~ allDocuments", allDocuments);
       const indices = this.models.map((x, i) => {
-        return { name: pluralize.plural(x.name), documents: allDocuments[i] };
+        if (isSequelasticModel(x)) {
+          return {
+            name: pluralize.plural(x.model.name),
+            documents: allDocuments[i],
+          };
+        } else {
+          return { name: pluralize.plural(x.name), documents: allDocuments[i] };
+        }
       });
-      // return indices;
       for (const index of indices) {
-        // console.log(index.documents[0]);
         const docs = index.documents.flat();
-        // console.log("🚀 ~ file: index.ts ~ line 80 ~ Sqlastic ~ sync ~ docs", docs);
-        this.elastic.bulk({ body: docs }).catch((err) => {
+        this.elastic.bulk({ body: docs, refresh: true }).catch((err) => {
           console.log("BULK ERR".red, err);
         });
       }
-      // this.elastic.updateByQuery({});
     } catch (err) {
       console.error(err);
       return false;
     }
     return true;
+  }
+
+  public async search(
+    index: "_all" | "*" | string,
+    query: string,
+    options?: SequelasticSearchOptions
+  ): Promise<[{ [key: string]: any }]>;
+
+  public async search(
+    index: "_all" | "*" | string,
+    query: string,
+    options?: SequelasticSearchOptionsWholeResponse
+  ): Promise<
+    elasticSearch.ApiResponse<Record<string, any>, Record<string, unknown>>
+  >;
+
+  public async search(
+    index: "_all" | "*" | string,
+    query: string,
+    options?: SequelasticSearchOptionsWholeResponse | SequelasticSearchOptions
+  ): Promise<
+    | [{ [key: string]: any }]
+    | elasticSearch.ApiResponse<Record<string, any>, Record<string, unknown>>
+  > {
+    try {
+      if (!query) {
+        return [{}];
+      }
+      const result = await this.elastic.search({
+        index: "_all",
+        body: {
+          aggs: {
+            byIndex: {
+              terms: {
+                field: "_index",
+              },
+            },
+          },
+          query: {
+            query_string: {
+              query: options?.fuzzy ? `${query}~` : query,
+              default_field: index,
+              fuzziness: options?.fuzziness,
+            },
+          },
+        },
+      });
+
+      if (options.wholeResponse) return result;
+      return result.body.hits.hits;
+    } catch (err) {
+      this.handleErrors(err);
+    }
+  }
+
+  public async customSearch(
+    params: elasticSearch.RequestParams.Search
+  ): Promise<
+    elasticSearch.ApiResponse<Record<string, any>, Record<string, unknown>>
+  > {
+    try {
+      const result = await this.elastic.search(params);
+      return result;
+    } catch (err) {
+      this.handleErrors(err);
+    }
+  }
+
+  private handleErrors(error: any): never {
+    if (error.body?.error) {
+      console.log("Sequelastic error:", error.body?.error);
+
+      throw new Error(error.body?.error);
+    }
+    console.log("Sequelastic error", error);
+
+    throw new Error(error);
   }
 }
 
